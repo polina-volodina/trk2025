bddacf3f911bb6fc7646d49b3235f6a52ff13a52
polina-volodina <polina.volodina2000@gmail.com>
2025-04-23 09:34:49 +0300

    Nüüd kõik töötab VisualStudios minu arvutis

---
+++ elektriskeem/skeem.ts
@@ -0,0 +1,17 @@
+class Resistor {
+    r: number;
+    constructor(r: number) {
+        this.r = r;
+    }
+    getCurrent(u: number): number {
+        return u / this.r;
+    }
+}
+
+
+let r1: Resistor = new Resistor(110);
+console.log(r1.getCurrent(5));
+let r2: Resistor = new Resistor(220);
+console.log(r2.getCurrent(5));
+let r3: Resistor = new Resistor(4700);
+console.log(r3.getCurrent(5));
